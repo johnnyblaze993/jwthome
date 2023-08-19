89f04d93b1f7ec537204cf52cbea1eb98e07f28b
Jonathan Alva <john.alvarez.93@gmail.com>
2023-08-19 17:34:11 -0500

    app working

---
+++ src/main/java/com/roxy/entities/Users.java
@@ -0,0 +1,15 @@
+package com.roxy.entities;
+
+import io.micronaut.data.annotation.Id;
+import io.micronaut.data.annotation.MappedEntity;
+import jakarta.validation.constraints.NotNull;
+
+// import java.util.UUID;
+
+@MappedEntity
+public record Users(
+        @Id @NotNull Long id,
+        @NotNull String username,
+        @NotNull String password) {
+
+}
