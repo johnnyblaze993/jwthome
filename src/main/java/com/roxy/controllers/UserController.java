15a2a13a5d7440b17feb1aca9050ada0ab6da4a1
Jonathan Alva <john.alvarez.93@gmail.com>
2023-08-22 12:43:42 -0500

    login working

--- src/main/java/com/roxy/controllers/UserController.java
+++ src/main/java/com/roxy/controllers/UserController.java
@@ -1,10 +1,15 @@
 package com.roxy.controllers;
 
+import com.roxy.dto.AuthenticationRequest;
 import com.roxy.entities.Users;
 import com.roxy.repositories.UserRepository;
+
+import io.micronaut.http.HttpResponse;
 import io.micronaut.http.annotation.*;
 import io.micronaut.scheduling.TaskExecutors;
 import io.micronaut.scheduling.annotation.ExecuteOn;
+
+import java.util.HashMap;
 //import java.util.List;
 import java.util.Optional;
 
@@ -43,5 +48,22 @@ public class UserController {
   @Delete("/{id}")
   public void delete(Long id) {
     userRepository.deleteById(id);
+  }
+
+  @Post("/login")
+  public HttpResponse<?> login(@Body AuthenticationRequest request) {
+    Users user = userRepository.findByUsernameAndPassword(request.getUsername(), request.getPassword());
+
+    if (user != null) {
+      // Return user data and a message
+      return HttpResponse.ok(new HashMap<String, Object>() {
+        {
+          put("user", user);
+          put("message", "Logged in");
+        }
+      });
+    }
+
+    return HttpResponse.notFound("User not found");
   }
 }
--- src/main/java/com/roxy/repositories/UserRepository.java
+++ src/main/java/com/roxy/repositories/UserRepository.java
@@ -1,6 +1,8 @@
 package com.roxy.repositories;
 
 import com.roxy.entities.Users;
+
+import io.micronaut.data.annotation.Query;
 import io.micronaut.data.jdbc.annotation.JdbcRepository;
 import io.micronaut.data.model.query.builder.sql.Dialect;
 import io.micronaut.data.repository.CrudRepository;
@@ -9,5 +11,7 @@ import io.micronaut.data.repository.CrudRepository;
 @JdbcRepository(dialect = Dialect.POSTGRES) // specify the dialect here
 public interface UserRepository extends CrudRepository<Users, Long> {
     // your repository methods
+    @Query("SELECT * FROM jwt_auth.users WHERE username = :username AND password = :password")
+    Users findByUsernameAndPassword(String username, String password);
 
 }
